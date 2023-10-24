e9f96373e60903f8c235f925f99bdf488cc839e1
Ash2901 <ashishashu2901@gmail.com>
2023-10-24 22:28:35 +0530

    Final Commit

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
-const heading = React.createElement("h1", { id: "heading", abc: "xyz" }, "Hello world by React");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const heading = React.createElement("div", { id: "heading", abc: "xyz" }, 
+[React.createElement("h1", {},"Hello world by React"), 
+React.createElement("h2", {}," Namaste React!")]);
 //console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //console.log(root);
